3723dcc64806bf853634b5c3094ca126d7ff578b
aman-sarkar <amansarkar199@gmail.com>
2024-08-22 02:35:07 +0530

    Change in db/tsconfig.ts

--- apps/user-app/components/BalanceCard.tsx
+++ apps/user-app/components/BalanceCard.tsx
@@ -23,7 +23,7 @@ export const BalanceCard = ({amount, locked}: {
         </div>
         <div className="flex justify-between border-b border-slate-300 py-2">
             <div>
-                Total Balance
+                Total Balance: 
             </div>
             <div>
                 {(locked + amount) / 100} INR
